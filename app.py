8918826350cf6848d86d61789d86f9e02da47609
maciejpiasecki <wrx75789@student.wsb.wroclaw.pl>
2023-05-15 22:02:35 +0200

    Dodano nowe pliki

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
+"""Module providingFunction printing python version."""
 from flask import Flask
+
 app = Flask(__name__)
 @app.route('/')
 def index():
+    """Function printing python version."""
     return '<h1>Hello WSB! Greetings from Flask!</h1>'
 if __name__ == "__main__":
     app.run(debug=True)
