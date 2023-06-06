f772f72107ae564c3d619ec5651daa5d2a0434a4
maciejpiasecki <wrx75789@student.wsb.wroclaw.pl>
2023-06-06 17:23:16 +0200

    tekst commita

--- app.py
+++ app.py
@@ -1,24 +1,10 @@
-<<<<<<< HEAD
-from flask import Flask, render_template
-from markupsafe import escape
-=======
+"""Module providingFunction printing python version."""
 from flask import Flask
->>>>>>> parent of 8918826 (Dodano nowe pliki)
+
 app = Flask(__name__)
-
-
 @app.route('/')
 def index():
-<<<<<<< HEAD
-    return render_template('index.html')
-
-
-@app.route('/hello/')
-@app.route('/hello/<name>')
-def hello(name=None):
-    return render_template('hello.html', name=name)
-=======
+    """Function printing python version."""
     return '<h1>Hello WSB! Greetings from Flask!</h1>'
 if __name__ == "__main__":
     app.run(debug=True)
->>>>>>> parent of 8918826 (Dodano nowe pliki)
