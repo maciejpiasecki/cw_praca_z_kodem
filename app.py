001dee5cd9b4ed8089989fa873d8896ebf4072b6
maciejpiasecki <wrx75789@student.wsb.wroclaw.pl>
2023-06-06 17:13:00 +0200

    tekst commita

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
+<<<<<<< HEAD
 from flask import Flask, render_template
 from markupsafe import escape
+=======
+from flask import Flask
+>>>>>>> parent of 8918826 (Dodano nowe pliki)
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
+<<<<<<< HEAD
     return render_template('index.html')
 
 
@@ -12,3 +17,8 @@ def index():
 @app.route('/hello/<name>')
 def hello(name=None):
     return render_template('hello.html', name=name)
+=======
+    return '<h1>Hello WSB! Greetings from Flask!</h1>'
+if __name__ == "__main__":
+    app.run(debug=True)
+>>>>>>> parent of 8918826 (Dodano nowe pliki)
