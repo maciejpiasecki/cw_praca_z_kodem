73427dd0f8fd0502d08abe6faa76ec0ff93643fa
vdi-terminal <vdi-terminal@EDU-WSB.local>
2023-06-18 11:29:58 +0200

    commit

--- app.py
+++ app.py
@@ -1,10 +1,7 @@
-"""Module providingFunction printing python version."""
 from flask import Flask
-
 app = Flask(__name__)
 @app.route('/')
 def index():
-    """Function printing python version."""
-    return '<h1>Hello WSB! Greetings from Flask!</h1>'
+ return '<h1>Hello WSB! Greetings from Flask & Docker!</h1>'
 if __name__ == "__main__":
-    app.run(debug=True)
+ app.run(debug=True)
--- cw_z_kodem/Makefile.bat
+++
@@ -1,6 +0,0 @@
-
-pip install -r requirements.txt
- flask run
-python -m flask run
-
-
